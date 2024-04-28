337a3f9799cb1c13dcffd9eb92069ec3c0c5a3d6
munmud <moontasir042@gmail.com>
2024-04-28 19:31:49 +0600

    fix mock test

--- users/tests/unit/test_handler.py
+++ users/tests/unit/test_handler.py
@@ -3,7 +3,7 @@ import os
 import boto3
 import uuid
 import pytest
-from moto import mock_dynamodb
+from moto import mock_aws
 from contextlib import contextmanager
 from unittest.mock import patch
 
@@ -19,10 +19,11 @@ def mock_uuid():
 
 @contextmanager
 def my_test_environment():
-    with mock_dynamodb():
+    with mock_aws():
         set_up_dynamodb()
         put_data_dynamodb()
         yield
+
 
 def set_up_dynamodb():
     conn = boto3.client(
@@ -41,6 +42,7 @@ def set_up_dynamodb():
             'WriteCapacityUnits': 1
         }
     )
+
 
 def put_data_dynamodb():
     conn = boto3.client(
@@ -63,6 +65,7 @@ def put_data_dynamodb():
         }
     )
 
+
 @patch.dict(os.environ, {'USERS_TABLE': USERS_MOCK_TABLE_NAME, 'AWS_XRAY_CONTEXT_MISSING': 'LOG_ERROR'})
 def test_get_list_of_users():
     with my_test_environment():
@@ -71,20 +74,21 @@ def test_get_list_of_users():
             apigw_get_all_users_event = json.load(f)
         expected_response = [
             {
+                'userid': UUID_MOCK_VALUE_JANE,
+                'name': 'Jane Doe',
+                'timestamp': '2021-03-30T17:13:06.516Z'
+            },
+            {
                 'userid': UUID_MOCK_VALUE_JOHN,
                 'name': 'John Doe',
                 'timestamp': '2021-03-30T21:57:49.860Z'
             },
-            {
-                'userid': UUID_MOCK_VALUE_JANE,
-                'name': 'Jane Doe',
-                'timestamp': '2021-03-30T17:13:06.516Z'
-            }
         ]
         ret = users.lambda_handler(apigw_get_all_users_event, '')
         assert ret['statusCode'] == 200
         data = json.loads(ret['body'])
         assert data == expected_response
+
 
 def test_get_single_user():
     with my_test_environment():
@@ -101,6 +105,7 @@ def test_get_single_user():
         data = json.loads(ret['body'])
         assert data == expected_response
 
+
 def test_get_single_user_wrong_id():
     with my_test_environment():
         from src.api import users
@@ -111,6 +116,7 @@ def test_get_single_user_wrong_id():
         ret = users.lambda_handler(apigw_event, '')
         assert ret['statusCode'] == 200
         assert json.loads(ret['body']) == {}
+
 
 @patch('uuid.uuid1', mock_uuid)
 @pytest.mark.freeze_time('2001-01-01')
@@ -127,6 +133,7 @@ def test_add_user():
         assert data['timestamp'] == '2001-01-01T00:00:00'
         assert data['name'] == expected_response['name']
 
+
 @pytest.mark.freeze_time('2001-01-01')
 def test_add_user_with_id():
     with my_test_environment():
@@ -134,13 +141,15 @@ def test_add_user_with_id():
         with open('./events/event-post-user.json', 'r') as f:
             apigw_event = json.load(f)
         expected_response = json.loads(apigw_event['body'])
-        apigw_event['body'] = apigw_event['body'].replace('}', ', \"userid\":\"123456789\"}')
+        apigw_event['body'] = apigw_event['body'].replace(
+            '}', ', \"userid\":\"123456789\"}')
         ret = users.lambda_handler(apigw_event, '')
         assert ret['statusCode'] == 200
         data = json.loads(ret['body'])
         assert data['userid'] == '123456789'
         assert data['timestamp'] == '2001-01-01T00:00:00'
         assert data['name'] == expected_response['name']
+
 
 def test_delete_user():
     with my_test_environment():
@@ -151,4 +160,3 @@ def test_delete_user():
         assert ret['statusCode'] == 200
         assert json.loads(ret['body']) == {}
 # Add your unit testing code here
-
